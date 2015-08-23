e8475dccac98118a4c6e6c0bbb7d35333ea42c67
Ciaran Evans <ciaran.evans@students.plymouth.ac.uk>
2015-08-24 00:29:59 +0100

    Added poo to player.cs

--- GBWestern/GBWestern/Player.cs
+++ GBWestern/GBWestern/Player.cs
@@ -49,7 +49,7 @@ namespace GBWestern
             //idleL.SpriteSheet = Content.Load<Texture2D>("Textures\\cartIdleL");
             //idleR.SpriteSheet = Content.Load<Texture2D>("Textures\\cartIdleR");
             //runL.SpriteSheet = Content.Load<Texture2D>("Textures\\cartRunL");
-            //runR.SpriteSheet = Content.Load<Texture2D>("Textures\\cartRunR");
+            //poorunR.SpriteSheet = Content.Load<Texture2D>("Textures\\cartRunR");
         }
 
         public void Update(GamePadState gP, GameTime gT, KeyboardState kB)
